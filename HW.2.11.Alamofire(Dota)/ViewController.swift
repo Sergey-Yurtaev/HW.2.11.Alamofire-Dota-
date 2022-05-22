d8d957c7ce338ea483bbeeec2f1ff346a9c5cff8
Sergey-Yurtaev <engineerk2@mail.ru>
2022-05-22 12:45:53 +0500

    Initial Commit

---
+++ HW.2.11.Alamofire(Dota)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW.2.11.Alamofire(Dota)
+//
+//  Created by Sergey Yurtaev on 22.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
